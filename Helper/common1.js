277add5996866a1b757f2a8c80a8286e02770b13
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-04-01 12:49:38 +0530

    helper

---
+++ Helper/common1.js
@@ -0,0 +1,8 @@
+const common1 = require("../maths");
+console.log(common1);
+
+const sum = common1.add(53, 5);
+console.log("Sum", sum);
+const sub = common1.sub(5, 5);
+console.log("Sub ", sub);
+// console.log(maths);
