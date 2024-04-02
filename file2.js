68b92d9c5b71b8a90c031f33f88e6305b32a9b65
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-04-02 12:18:16 +0530

    read by async await

--- file2.js
+++ file2.js
@@ -1,8 +1,23 @@
+// const fs = require("fs");
+// fs.readFile("./log.txt", "utf-8", (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data, "data");
+//   }
+// });
 const fs = require("fs");
-fs.readFile("./log.txt", "utf-8", (error, data) => {
-  if (error) {
+async function file() {
+  try {
+    await fs.readFile("./log.txt", "utf-8", (error, data) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(data, "data");
+      }
+    });
+  } catch (error) {
     console.log(error);
-  } else {
-    console.log(data, "data");
   }
-});
+}
+file();
