faf83be23b6eadb03296a5261aaf5cae5d0f9d18
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-05-01 11:59:36 +0530

    for loop

--- vue_project/src/App.vue
+++ vue_project/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <MyHome/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>  -->
+  <!-- <MyHome/>  -->
+  <!-- <Home/> -->
+  <MyText/>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import MyHome from './components/MyHome.vue'
-
+import MyText from './components/MyText.vue'
+// import MyHome from './components/MyHome.vue'
+// import Home from './components/Home.vue'
 
 
 export default {
   name: 'App',
   components: {
-    MyHome
+    // MyHome,
+    // Home
+    MyText
   }
 }
 </script>
---
+++ vue_project/src/components/MyText.vue
@@ -0,0 +1,51 @@
+<template>
+<h1 v-if='check'>{{ heading }}</h1>    
+<p v-else>{{ subHeading     }}</p>
+<h2>Sum {{ a+b }}</h2>
+<h2>Sum from function  {{ z}}</h2>
+
+<h2>{{ students[0] }}</h2>
+
+<h1 v-for="(student,index) in students" :key= "index" >{{ student }}</h1>
+
+
+<input id ="btn" type="button" value="Click" @click="add()"/> 
+
+</template>
+
+<script>
+    export default {
+        name:'MyText',
+        data(){
+            return {
+                heading:"Welcome to Bharat",
+                subHeading:"Welcome to UP",
+                check:true,
+                a:5,b:4,
+                z:0,
+                students:[
+                    'ram','mohan','gettaa'
+                ]
+                // result : a+b
+            }
+
+        },
+        methods:{
+            add(){
+            //    alert ('ClickMe')
+               this.z = this.a+this.b
+               this.heading = "RD"
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1{
+    color:rgb(223, 222, 239)d;
+}
+#btn{
+        color: aqua;
+}
+</style>
