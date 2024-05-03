73fa4c44fe4be7fc06fbb7130d185a394fe8c5b7
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-05-03 12:41:01 +0530

    front ens

--- RDproject/api-project/controllers/UserController.js
+++ RDproject/api-project/controllers/UserController.js
@@ -1,7 +1,9 @@
 const User = require("../models/User");
 
-const createuser = (req, res) => {
+const createuser = async (req, res) => {
   console.log(req.body);
+  let user = new User(req.body);
+  await user.save();
   res.send({ message: "data received " });
   console.log(createuser);
 };
